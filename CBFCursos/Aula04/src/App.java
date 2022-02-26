d167df34341b01884e9c51d28f8532f5d26904a9
unknown <geovanidebastiani32@gmail.com>
2022-02-25 21:24:34 -0300

    Aula04

--- CBFCursos/Aula04/src/App.java
+++ CBFCursos/Aula04/src/App.java
@@ -1,5 +1,26 @@
+// Switch e Cases
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        try (Scanner myObj = new Scanner(System.in)) {
+            System.out.print("Digite a posição: ");
+            int num = myObj.nextInt();
+            
+            switch (num){
+                case 1:
+                    System.out.print("Primeiro Lugar");
+                    break;
+                case 2:
+                    System.out.print("Segundo Lugar");
+                    break;
+                case 3:
+                    System.out.print("Terceiro Lugar");
+                    break;
+                default:
+                    System.out.print("Não entrou no pódio :(");
+            }
+        }
     }
 }
