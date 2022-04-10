4c0307bf3063e18d2a1d1fbb1e9448d14c659999
Geovani Debastiani <geovanidebastiani32@gmail.com>
2022-04-10 10:33:14 -0300

    Aprimorando exe

--- Desafios/Retição com Do/src/App.java
+++ Desafios/Retição com Do/src/App.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
         int cc = 1;
+        String resp;
 
+        Scanner scan = new Scanner(System.in);
+        
         do {
             System.out.println("Hello, World!");
-            cc += 1;
-        } while (cc <= 6);
+            
+            System.out.print("Deseja ver mais um Hello? R(S/N): ");
+            resp = scan.nextLine().toUpperCase();
 
 
+        } while (resp.equals("S"));
     }
 }
